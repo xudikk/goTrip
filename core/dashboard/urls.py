6bc8f82c2559a31bcc10f682d162a805e3cfdf75
Johadev05 <javohirsidikxonov@gmail.com>
2023-08-22 16:00:16 +0500

    Connect static files and templates

--- core/dashboard/urls.py
+++ core/dashboard/urls.py
@@ -3,7 +3,9 @@
 #  Please contact before making any changes
 #
 #  Tashkent, Uzbekistan
+from django.urls import path, include
+from .view import index
 
 urlpatterns = [
-
+    path("", index, name='home')
 ]
--- core/dashboard/view.py
+++ core/dashboard/view.py
@@ -3,3 +3,10 @@
 #  Please contact before making any changes
 #
 #  Tashkent, Uzbekistan
+
+from django.shortcuts import redirect, render
+
+
+def index(request):
+
+    return render(request, 'pages/index.html')
