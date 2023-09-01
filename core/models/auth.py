7b0c4daaf381c828b09ae90997a6da455be8043d
xudikk <xudikk.1@gmail.com>
2023-09-01 23:45:23 +0500

    aaa
    saaa

--- core/models/auth.py
+++ core/models/auth.py
@@ -35,6 +35,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=255, null=True)
     email = models.CharField(max_length=255, null=True)
     avatar = models.CharField(max_length=255, null=True)
+    gender = models.BooleanField(default=True)
 
     is_test = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
